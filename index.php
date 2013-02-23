9da0d8a6f5d751bbea670b6e386d1e93b54ae7e8
Rasmus Larsson <meister@rasmuslarsson.se>
2013-02-23 19:17:50 +0100

    Basic Doctrine configuration

--- index.php
+++ index.php
@@ -6,24 +6,44 @@ $app = new Silex\Application();
 
 $app['debug'] = true;
 
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'    => 'pdo_mysql',
+        'host'      => 'localhost',
+        'dbname'    => 'cabbage',
+        'user'      => 'root',
+        'password'  => '',
+        'charset'   => 'utf8'
+    )
+));
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
+/**
+ * Main view, renders a list of all galleries
+ */
 $app->get('/', function() use ($app) {
     return $app['twig']->render('overview.html.twig', array(
         'name' => 'Blarg'
     ));
 });
 
+/**
+ * POST-request to create a new gallery, redirects to new gallery
+ */
 $app->post('/', function() use ($app) {
     return $app['twig']->render('overview.html.twig', array(
         'messages' => 'Gallery created'
     ));
 });
 
+/**
+ * Shows a single image
+ */
 $app->get('/view/{name}', function($name) use($app) {
     return $app['twig']->render('view.html.twig', array(
         'name' => $name
