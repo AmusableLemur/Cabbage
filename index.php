03c81760b36a2eb49e5599f3ca12d750fde9d2e3
Rasmus Larsson <meister@rasmuslarsson.se>
2013-02-23 23:13:01 +0100

    Configured Doctrine DBAL for SQLite

--- index.php
+++ index.php
@@ -8,6 +8,13 @@ $app = new Silex\Application();
 
 $app['debug'] = true;
 
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => __DIR__.'/app.db',
+    ),
+));
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
