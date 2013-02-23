41f6d56a6d186b001e961bdf7c80481a6b0aeadb
Rasmus Larsson <meister@rasmuslarsson.se>
2013-02-23 19:36:59 +0100

    Added loading of galleries

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+define('BASE_DIR', 'galleries/');
+
 require_once __DIR__.'/vendor/autoload.php'; 
 
 $app = new Silex\Application();
@@ -16,7 +18,15 @@ $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
  * Main view, renders a list of all galleries
  */
 $app->get('/', function() use ($app) {
-    return $app['twig']->render('overview.html.twig');
+    $galleries = array_filter(scandir(BASE_DIR), function ($name) {
+        $directory = BASE_DIR.$name;
+
+        return is_dir($directory) && !in_array($name, array('.', '..'));
+    });
+
+    return $app['twig']->render('overview.html.twig', array(
+        'galleries' => $galleries
+    ));
 });
 
 /**
