f106464da09bf0cddceb26daa614268acbddfc24
Rasmus Larsson <meister@rasmuslarsson.se>
2013-02-23 21:17:13 +0100

    Added parsing of galleries

--- index.php
+++ index.php
@@ -18,11 +18,45 @@ $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
  * Main view, renders a list of all galleries
  */
 $app->get('/', function() use ($app) {
-    $galleries = array_filter(scandir(BASE_DIR), function ($name) {
-        $directory = BASE_DIR.$name;
+    $galleries = array_map(
+        function ($name) {
+            $directory = BASE_DIR.$name;
 
-        return is_dir($directory) && !in_array($name, array('.', '..'));
-    });
+            // Make sure we are actually dealing with a folder
+            if (!is_dir($directory) || in_array($name, array('.', '..'))) {
+                return null;
+            }
+
+            $directory = dir($directory);
+            $image = null;
+
+            // Load all files in the folder one by one, store it and break the
+            // loop if it's an image.
+            while (($file = $directory->read()) !== false) {
+                $extension = explode('.', $file);
+                $extension = array_pop($extension);
+                $extension = strtolower($extension);
+
+                if (in_array($extension, array('jpg', 'png', 'gif'))) {
+                    $image = $file;
+
+                    break;
+                }
+            }
+
+            // If no image was found the folder is not a gallery
+            if ($image === null) {
+                return null;
+            }
+
+            $gallery = array();
+            $gallery['name'] = $name;
+            $gallery['image'] = $image;
+
+            return $gallery;
+        },
+        scandir(BASE_DIR)
+    );
 
     return $app['twig']->render('overview.html.twig', array(
         'galleries' => $galleries
