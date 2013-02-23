39ebdf7173cdcfa6b01fd15998ea7c49e9dedb52
Rasmus Larsson <meister@rasmuslarsson.se>
2013-02-23 19:29:02 +0100

    Pivot!
    Maybe galleries shouldn't be stored in database

--- index.php
+++ index.php
@@ -6,17 +6,6 @@ $app = new Silex\Application();
 
 $app['debug'] = true;
 
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options' => array(
-        'driver'    => 'pdo_mysql',
-        'host'      => 'localhost',
-        'dbname'    => 'cabbage',
-        'user'      => 'root',
-        'password'  => '',
-        'charset'   => 'utf8'
-    )
-));
-
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
@@ -27,32 +16,15 @@ $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
  * Main view, renders a list of all galleries
  */
 $app->get('/', function() use ($app) {
-    return $app['twig']->render('overview.html.twig', array(
-        'name' => 'Blarg'
-    ));
-});
-
-/**
- * POST-request to create a new gallery, redirects to new gallery
- */
-$app->post('/', function() use ($app) {
-    return $app['twig']->render('overview.html.twig', array(
-        'messages' => 'Gallery created'
-    ));
+    return $app['twig']->render('overview.html.twig');
 });
 
 /**
  * Shows a single image
  */
-$app->get('/view/{name}', function($name) use($app) {
+$app->get('/view/{gallery}/{image}', function($gallery, $image) use($app) {
     return $app['twig']->render('view.html.twig', array(
-        'name' => $name
-    ));
-});
-
-$app->post('/view/{name}', function() use ($app) {
-    return $app['twig']->render('overview.html.twig', array(
-        'messages' => 'Image uploaded'
+        'image' => $image
     ));
 });
 
